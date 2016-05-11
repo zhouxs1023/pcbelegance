bbc1e8a19efac0638fd2a96844284898e621817d
Tim Worthington <Tim Worthington@localhost>
2016-05-11 19:13:42 +1000

    first

---
+++ icons.h
@@ -0,0 +1,15 @@
+
+
+
+#ifndef _ICONS
+
+#define _ICONS
+
+extern  int32 NrBitMaps;
+extern  HICON BitMaps[32];
+
+void LoadBitMaps(void);
+void DestroyBitMaps(void);
+
+#endif
+
---
+++ special_jwlfewefjo.h
@@ -0,0 +1,7 @@
+
+#ifdef special_jwlfewefjo_h
+
+#define special_jwlfewefjo_h
+
+#endif
+
